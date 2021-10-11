bf418d89fb3263857380e5043458f95fa4147cc1
Andreas Lönnermark <andreas.lonnermark@gmail.com>
2021-10-11 13:50:15 +0200

    Added Todo class + tests.

---
+++ AssignmentProject.Tests/TodoTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AssignmentProject.Model;
+
+namespace AssignmentProject.Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public void ShouldCreateNewTodo()
+        {
+            Todo testTask = new Todo(0, "test");
+            Assert.Equal(0, testTask.TodoId);
+            Assert.Equal("test", testTask.Description);
+            Assert.False(testTask.Done);
+        }
+    }
+}
---
+++ AssignmentProject/Model/Todo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentProject.Model
+{
+    public class Todo
+    {
+        private readonly int todoId;
+        private String description;
+        private bool done;
+        private Person assignee;
+
+        public Todo(int todoId, String description)
+        {
+            this.todoId = todoId;
+            this.description = description;
+            done = false;
+        }
+
+        public int TodoId
+        {
+            get
+            {
+                return todoId;
+            }
+        }
+
+        public String Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public bool Done
+        {
+            get
+            {
+                return done;
+            }
+            set
+            {
+                done = value;
+            }
+        }
+
+        public Person Assignee
+        {
+            get
+            {
+                return assignee;
+            }
+            set
+            {
+                assignee = value;
+            }
+        }
+    }
+}
