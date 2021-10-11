7a6d0ec4c1edb2db083d19726d97ac45825df4ba
Andreas Lönnermark <andreas.lonnermark@gmail.com>
2021-10-11 15:42:40 +0200

    Added TodoItems class + tests.

---
+++ AssignmentProject.Tests/TodoItemsTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AssignmentProject.Data;
+using AssignmentProject.Model;
+
+namespace AssignmentProject.Tests
+{
+    public class TodoItemsTests
+    {
+        [Fact]
+        public void ShouldReturnNumberofTodoItems()
+        {
+            TodoItems.Clear();
+            Assert.Equal(0, TodoItems.Size());
+        }
+
+        [Fact]
+        public void ShouldReturnTodoItemsArray()
+        {
+            Person[] testTodoItems = new Person[People.FindAll().Length];
+            Assert.Equal(testTodoItems, People.FindAll());
+        }
+
+        [Fact]
+        public void ShouldFindTodoItemById()
+        {
+            TodoItems.Clear();
+            Todo testTodo = TodoItems.AddNewTodo("test");
+            Todo searchResult = TodoItems.FindById(testTodo.TodoId);
+            Assert.Equal(1, TodoItems.Size());
+            Assert.Equal(1, testTodo.TodoId);
+            Assert.Equal(testTodo.TodoId, TodoItems.FindAll()[0].TodoId);
+            Assert.Equal(testTodo.Description, searchResult.Description);
+            Assert.Throws<KeyNotFoundException>(() => TodoItems.FindById(-1));
+        }
+
+        [Fact]
+        public void ShouldAddNewTodo()
+        {
+            Todo createdTodo = TodoItems.AddNewTodo("test2");
+            Assert.Equal("test2", createdTodo.Description);
+
+        }
+
+        [Fact]
+        public void ShouldClearTodoItemsArray()
+        {
+            TodoItems.Clear();
+            Assert.Equal(0, TodoItems.Size());
+        }
+    }
+}
---
+++ AssignmentProject/Data/TodoItems.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssignmentProject.Model;
+
+namespace AssignmentProject.Data
+{
+    public class TodoItems
+    {
+        private static Todo[] todoItems = new Todo[0];
+
+        public static int Size()
+        {
+            return todoItems.Length;
+        }
+
+        public static Todo[] FindAll()
+        {
+            return todoItems;
+        }
+
+        public static Todo FindById(int todoId)
+        {
+            if (todoItems.Length == 0)
+            {
+                throw new NullReferenceException();
+            }
+            for (int i = 0; i < todoItems.Length; i++)
+            {
+                if (todoItems[i].TodoId == todoId)
+                {
+                    return todoItems[i];
+                }
+            }
+            throw new KeyNotFoundException("No task with that id");
+        }
+
+        public static Todo AddNewTodo(String description)
+        {
+            Todo newTodoItem = new Todo(TodoSequencer.nextTodoId(), description);
+            Todo[] temptodoItems = new Todo[todoItems.Length + 1];
+            for (int i = 0; i < todoItems.Length; i++)
+            {
+                temptodoItems[i] = todoItems[i];
+            }
+            temptodoItems[temptodoItems.Length - 1] = newTodoItem;
+            todoItems = new Todo[temptodoItems.Length];
+            for (int i = 0; i < temptodoItems.Length; i++)
+            {
+                todoItems[i] = temptodoItems[i];
+            }
+            return newTodoItem;
+        }
+
+        public static void Clear()
+        {
+            todoItems = new Todo[0];
+            TodoSequencer.reset();
+        }
+    }
+}
