15b69b23c654421dbaa343007e9334858f538279
Andreas Lönnermark <andreas.lonnermark@gmail.com>
2021-10-11 14:00:21 +0200

    Added class PersonSequencer + tests.

---
+++ AssignmentProject.Tests/PersonSequencerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AssignmentProject.Data;
+
+namespace AssignmentProject.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        public void PersonIdTests()
+        {
+            Assert.Equal(1, PersonSequencer.nextPersonId());
+            Assert.Equal(2, PersonSequencer.nextPersonId());
+            Assert.Equal(3, PersonSequencer.nextPersonId());
+
+            PersonSequencer.reset();
+            Assert.Equal(1, PersonSequencer.nextPersonId());
+
+            PersonSequencer.reset();
+            PersonSequencer.reset();
+            Assert.Equal(1, PersonSequencer.nextPersonId());
+        }
+    }
+}
---
+++ AssignmentProject/Data/PersonSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentProject.Data
+{
+    public class PersonSequencer
+    {
+        private static int personId = 0;
+
+        public static int nextPersonId()
+        {
+            return ++personId;
+        }
+
+        public static void reset()
+        {
+            personId = 0;
+        }
+    }
+}
