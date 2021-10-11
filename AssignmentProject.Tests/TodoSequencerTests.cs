0534a7cf2e3ad727981ecdaca2930b842c8aa740
Andreas Lönnermark <andreas.lonnermark@gmail.com>
2021-10-11 14:05:07 +0200

    Added class TodoSequencer + tests.

---
+++ AssignmentProject.Tests/TodoSequencerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using AssignmentProject.Data;
+
+namespace AssignmentProject.Tests
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void TodoIdTests()
+        {
+            Assert.Equal(1, TodoSequencer.nextTodoId());
+            Assert.Equal(2, TodoSequencer.nextTodoId());
+            Assert.Equal(3, TodoSequencer.nextTodoId());
+
+            TodoSequencer.reset();
+            Assert.Equal(1, TodoSequencer.nextTodoId());
+
+            TodoSequencer.reset();
+            TodoSequencer.reset();
+            Assert.Equal(1, TodoSequencer.nextTodoId());
+        }
+    }
+}
---
+++ AssignmentProject/Data/TodoSequencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentProject.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoId = 0;
+
+        public static int nextTodoId()
+        {
+            return ++todoId;
+        }
+
+        public static void reset()
+        {
+            todoId = 0;
+        }
+    }
+}
